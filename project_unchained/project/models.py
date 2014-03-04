640b1c22ed82305d192e3404f0ef618d199704d6
Joonas Jauhiainen <joonas.jauhiainen@eficode.com>
2014-03-04 23:16:18 +0200

    Added project model and some tests.

--- project_unchained/project/models.py
+++ project_unchained/project/models.py
@@ -1,3 +1,30 @@
+# -*- coding: utf-8 -*-
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.contrib import admin
 # Create your models here.
+
+class Project(models.Model):
+    _project_unix_name = models.CharField(max_length=14, blank=False, unique=True)
+    project_name = models.CharField(max_length=14, blank=False, unique=True)
+    members = models.ManyToManyField(User)
+    project_description = models.TextField()
+    
+    
+    def save(self, *args, **kwargs):
+        if not self._project_unix_name and self.project_name:
+            self.project_unix_name = self.format_to_unix_name(self.project_name)
+        super(Project, self).save(*args, **kwargs)
+    
+    @property
+    def project_unix_name(self):
+        return self._project_unix_name
+    
+    @project_unix_name.setter
+    def project_unix_name(self, value):
+        if value:
+            self._project_unix_name = self.format_to_unix_name(value)
+            
+    def format_to_unix_name(self, value):
+        value = value.lower().replace('ä', 'a').replace('å', 'a').replace('ö', 'o').replace(' ', '_')
+        return value
--- project_unchained/project/tests.py
+++ project_unchained/project/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
-
+from django.core.exceptions import ValidationError
+from utils.testhelper import validate
+from models import Project
+from django.contrib.auth.models import User
 # Create your tests here.
+
+class ProjectTestCase(TestCase):
+    
+    def setUp(self):
+        self.user1 = User.objects.create(username="user1", password="passwd", email="user1@test.fi")
+        self.user1.save()
+        self.user2 = User.objects.create(username="user2", password="passwd", email="user2@test.fi").save()
+        self.project1 = Project(project_name="Project 1")
+        self.project1.save()
+        self.project1.members.add(self.user1)
+    
+    def test_project1_should_have_user1_as_member(self):
+        self.assertTrue(len(self.project1.members.all()) == 1)
